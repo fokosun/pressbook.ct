6e3326baa56612927fcee923001e530e1ad39fe4
fokosun <okosunuzflorence@gmail.com>
2020-09-12 00:52:20 -0600

    better form validation

--- question_3/astronaut_controller.php
+++ question_3/astronaut_controller.php
@@ -5,18 +5,20 @@ require __DIR__ . '/vendor/autoload.php';
 use Nasa\Astronaut;
 
 //form validation
-if ($_POST['name'] == "" || $_POST['weight'] == "" || !is_numeric($_POST['weight'])) {
-	header("Location: http://localhost:8081/?nameErr=true&weightErr=true");
-}
-
-$astronaut = new Astronaut([
-	'name' => $_POST['name'],
-	'weight' => $_POST['weight']
-]);
-
-if ($astronaut->save()) {
-	header("Location: http://localhost:8081/?success=true");
+if (!isset($_POST['name']) || $_POST['name'] === "" || count($_POST['name']) == 0) {
+	header("Location: http://localhost:8081/?nameErr=true");
+} else if (!isset($_POST['weight']) || $_POST['weight'] === "") {
+	header("Location: http://localhost:8081/?weightErr=true");
 } else {
-	var_dump('There were errors.');
-	die();
+	$astronaut = new Astronaut([
+		'name' => $_POST['name'],
+		'weight' => $_POST['weight']
+	]);
+
+	if ($astronaut->save()) {
+		header("Location: http://localhost:8081/?success=true");
+	} else {
+		var_dump('There were errors.');
+		die();
+	}
 }
