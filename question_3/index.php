7ec0da2b357f5a799b0641c613f78926496c687a
fokosun <okosunuzflorence@gmail.com>
2020-09-15 09:24:29 -0600

    inline styling to display form errors

--- question_3/index.php
+++ question_3/index.php
@@ -5,12 +5,12 @@
 <form action="astronaut_controller.php" method="post">
 	Name: <input type="text" name="name">
     <label>
-        <?php if(isset($_GET['nameErr'])) { echo 'Astronaut name is required. '; } ?>
+        <?php if(isset($_GET['nameErr'])) { echo '<p style="color: red; font-size: xx-small;"> *Astronaut name is required. </p>'; } ?>
     </label>
     <br>
 	Weight: <input type="text" name="weight">
     <label>
-		<?php if(isset($_GET['weightErr'])) { echo 'Astronaut weight is required as a number. '; } ?>
+		<?php if(isset($_GET['weightErr'])) { echo '<p style="color: red; font-size: xx-small;"> *Astronaut weight is required as a number. </p>'; } ?>
     </label>
 	<br>
 	<input type="submit">
